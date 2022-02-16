998f34b784d60c2ded097a9ea0bae8d81f9e5e1f
Abhiraj <singhabhiraj915@gmail.com>
2022-02-16 16:02:11 +0530

    defined theme for styled components and created a few styled components

--- src/components/show/Cast.js
+++ src/components/show/Cast.js
@@ -15,7 +15,7 @@ function Cast({ cast }) {
           </div>
           <div className="actor">
             <span>
-              <span className="bold">{person.name}</span>| {character.name}{' '}
+              <span className="bold">{person.name}</span> | {character.name}{' '}
               {voice ? '| Voice' : ''}
             </span>
           </div>
